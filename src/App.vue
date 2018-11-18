752669925aa471aecd9114ec1bb447d60a9cc2b9
AshleyAbramowiczGibbs <ashleyabramo2@gmail.com>
2018-11-18 12:07:56 -0600

    Make app look good

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,35 @@
 <template>
   <div id="app">
     <div id="nav">
+      <nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul class="navbar-nav">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Features</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Pricing</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown link
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+    </ul>
+  </div>
+</nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
@@ -27,5 +56,9 @@
 
 #nav a.router-link-exact-active {
   color: #42b983;
+}
+
+body {
+  background-image: url("../src/assets/Dresses.jpg");
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,33 +1,88 @@
 <template>
   <div class="home">
-    <h1>{{ message }}</h1>
-    <div v-for="product in products">
-      <h2>{{ product.name }}</h2>
-      <p>{{ product.description }}</p>
+   <!--  <img src="../assets/Dresses.jpg" alt=""> -->
+    <div class="circular" v-bind:style="{ 'background-image': '../assets/Dresses.jpg' }"></div>
+
+    <div class="container">
+      <div class="jumbotron">
+        <h1 class="display-4">Bridesmaid Dress Resale</h1>
+        <p class="lead">
+          This is a simple hero unit, a simple jumbotron-style component for
+          calling extra attention to featured content or information.
+        </p>
+        <hr class="my-4" />
+        <p>
+          It uses utility classes for typography and spacing to space content
+          out within the larger container.
+        </p>
+        <a class="btn btn-primary btn-lg" href="#" role="button"
+          >Buy a dress!</a
+        >
+        <br />
+        <a class="btn btn-primary btn-lg" href="#" role="button"
+          >Sell a dress!</a
+        >
+      </div>
+      <div class="row">
+        <div v-for="product in products" class="col-md-4 mb-2">
+          <div class="card">
+            <img
+              class="card-img-top"
+              v-bind:src="getImageFromProduct(product)"
+              alt="Card image cap"
+            />
+            <div class="card-body">
+              <h5 class="card-title">{{ product.name }}</h5>
+              <p class="card-text">Chef: {{ product.description }}</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <style>
+body {
+  font-family: Candara, Calibri, Segoe, "Segoe UI", Optima, Arial, sans-serif;
+}
+
+.jumbotron {
+  opacity: 0.8;
+}
+
+.btn-primary {
+  background: #b76e79;
+  border: white;
+}
 </style>
 
 <script>
-  let axios = require("axios");
+let axios = require("axios");
 export default {
-    data: function() {
-      return {
-        message: "Welcome to Vue.js!",
-        products: []
-      };
-    },
-    created: function() {
-      axios.get("http://localhost:3000/api/products").then(function(response) {
+  data: function() {
+    return {
+      message: "Welcome to Vue.js!",
+      products: []
+    };
+  },
+  created: function() {
+    axios.get("http://localhost:3000/api/products").then(
+      function(response) {
         console.log(response.data);
         this.products = response.data;
       }.bind(this)
-      );
-    },
-    methods: {},
-    computed: {}
+    );
+  },
+  methods: {
+    getImageFromProduct: function(product) {
+      if (product.image[0]) {
+        return product.image[0].url;
+      } else {
+        return "https://cdn7.bigcommerce.com/s-yd0nd8/images/stencil/1280x1280/products/169/5820/Revelry_074__56124.1540512985.jpg?c=2&imbypass=on";
+      }
+    }
+  },
+  computed: {}
 };
 </script>
